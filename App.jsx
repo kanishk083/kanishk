ac7202ae15546c242223da0564387506252f29ec
Kanishk <146994401+kanishk083@users.noreply.github.com>
2024-11-03 17:57:24 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,18 +1,46 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Footer from "./assets/components/Footer"
 import Main from "./assets/components/Main"
 import SideBar from "./assets/components/SideBar"
 
 
 function App() {
-  const [showModel,setshowModel] = useState(false)
+
+  const [showModal, setshowModal] = useState(false)
+
+
+  function handleToggleModal() {
+    setshowModal(!showModal)
+  }
+
+  useEffect(() => {
+    async function APIFetch() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = `GET https://api.nasa.gov/planetary/apod` +
+        `?api_key=${NASA_KEY}`;
+        
+
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log("DATA\n", data)
+      } catch (err) {
+        console.log("Error",err.message)
+      }
+
+    }
+    APIFetch()
+  }, [])
+
+
+
   return (
     <>
-    <Main />
-    {showModel &&
-    (<SideBar />)}
-    <Footer />
-    
+      <Main />
+      {showModal && (
+        <SideBar />
+      )}
+      <Footer handleToggleModal={handleToggleModal} />
     </>
   )
 }
